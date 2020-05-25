dd8a07227257f3e6fdab46147cb784ada0f1548d
zhang <258983130@qq.com>
2020-05-25 21:28:04 +0800

    fit win linux

--- mkdir.py
+++ mkdir.py
@@ -1,13 +1,19 @@
 import os
 import re
-
+import sys
 
 def replace_line(x):
     return re.sub(r"[ :-]", "_", str(x))
 
+def syst():
+    x = input("选择你的操作系统:\n 1.win\n 2.linux\n 3.mac\n ")
+    if x == "1":
+        return "win"
+    else: 
+        return "other"
 
 
-def coures1_dir(course1):
+def coures1_dir(course1, sys):
     for course in course1.keys():
         c_name = replace_line(course)
         for week in course1[course].keys():
@@ -19,25 +25,32 @@ def coures1_dir(course1):
                     if type(c) is list:
                         for x in c:
                             cl_name = replace_line(x)
-                            path = "./{}/{}/{}/{}".format(c_name,
-                                                          w_name, str(cls_name), cl_name)
+                            if sys == "win":
+                                path = ".\\{}\\{}\\{}\\{}".format(c_name, w_name, str(cls_name), cl_name)
+                            else:
+                                path = "./{}/{}/{}/{}".format(c_name, w_name, str(cls_name), cl_name)
                             # print("list: {} \n {} ".format(c, path))
                             os.makedirs(path)
                     else:
                         cl_name = replace_line(c)
-                        path = "./{}/{}/{}/{}".format(c_name,
-                                                      w_name, str(cls_name), cl_name)
+                        if sys == "win":
+                            path = ".\\{}\\{}\\{}\\{}".format(c_name, w_name, str(cls_name), cl_name)
+                        else:
+                            path = "./{}/{}/{}/{}".format(c_name, w_name, str(cls_name), cl_name)
                         # print("no list: {} \n {} ".format(c, path))
                         os.makedirs(path)
             else:
                 cls_name = replace_line(cls)
-                path = "./{}/{}/{}".format(c_name, w_name, cls_name)
+                if sys == "win":
+                    path = ".\\{}\\{}\\{}".format(c_name, w_name, cls_name)
+                else:
+                    path = "./{}/{}/{}".format(c_name, w_name, cls_name)
                 # print("no dict:  ", path)
                 os.makedirs(path)
 
 
 
-def course2_dir(course1):
+def course2_dir(course1, sys):
     for course in course1.keys():
         c_name = replace_line(course)
         for week in course1[course].keys():
@@ -52,19 +65,23 @@ def course2_dir(course1):
                             if type(cl) is list:
                                 for x in cl:
                                     cl_name = replace_line(x)
-                                    path = "./{}/{}/{}/{}".format(c_name,
-                                                                  w_name, str(cls_name), cl_name)
+                                    if sys == "win":
+                                        path = ".\\{}\\{}\\{}\\{}".format(c_name, w_name, str(cls_name), cl_name)
+                                    else:
+                                        path = "./{}/{}/{}/{}".format(c_name, w_name, str(cls_name), cl_name) 
                                     # print("list: {} \n {} ".format(c, path))
                                     os.makedirs(path)
                             else:
                                 cl_name = replace_line(x)
-                                path = "./{}/{}/{}/{}".format(c_name,
-                                                              w_name, str(cls_name), cl_name)
+                                if sys == "win":
+                                    path = ".\\{}\\{}\\{}\\{}".format(c_name, w_name, str(cls_name), cl_name)
+                                else:
+                                    path = "./{}/{}/{}/{}".format(c_name, w_name, str(cls_name), cl_name)
                                 # print("no list: {} \n {} ".format(c, path))
                                 os.makedirs(path)
 
 
-def course3_dir(course3):
+def course3_dir(course3, sys):
     for course in course3.keys():
         c_name = replace_line(course)
         vls = course3[course]
@@ -72,14 +89,22 @@ def course3_dir(course3):
             if type(i) is dict:
                 w_name = replace_line(i.keys().__iter__().__next__())
                 cls_name = replace_line(i.values())
-                path = "./{}/{}/{}".format(c_name, w_name, cls_name)
+                if sys == "win":
+                    path = ".\\{}\\{}\\{}".format(c_name, w_name, cls_name)
+                else:
+                    path = "./{}/{}/{}".format(c_name, w_name, cls_name)
                 os.makedirs(path)
             else:
                 w_name = replace_line(i)
-                path = "./{}/{}".format(c_name, w_name)
+                if sys == 'win':
+                    path = ".\\{}\\{}".format(c_name, w_name)
+                else:
+                    path = "./{}/{}".format(c_name, w_name)
+                os.makedirs(path)
 
 
-if __name__ == "__main__":
+def run():
+    sys = syst()
     coursera = {
         "Foundations of strategic business analytics":
         {
@@ -160,12 +185,16 @@ if __name__ == "__main__":
     key_lst = list(coursera.keys())
     
     course1 = {key_lst[0]:coursera[key_lst[0]]}
-    coures1_dir(course1)
+    coures1_dir(course1, sys)
 
     course2 = {key_lst[1]:coursera[key_lst[1]]}
-    course2_dir(course2)
+    course2_dir(course2, sys)
 
     course3 = {key_lst[2]:coursera[key_lst[2]]}
-    course3_dir(course3)
+    course3_dir(course3, sys)
 
     print("create dirtory successfully")
+
+if __name__ == "__main__":
+    run()
+    
